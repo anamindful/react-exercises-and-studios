fc63eca04f6bdf8dba94178da27960ca8b2c0875
Anastasia Collins <anastasiacollins@Anastasias-MacBook-Air.local>
2023-08-07 20:18:24 -0500

    studio 2

--- part2/studio/src/components/RateARecipe.js
+++ part2/studio/src/components/RateARecipe.js
@@ -1,7 +1,15 @@
 let stars = ["⭐", "⭐ ⭐", "⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐", "⭐ ⭐ ⭐ ⭐ ⭐"];
 
-function RateARecipe() {
-  return stars;
+function RateARecipe(props) {
+  return (
+    <div>
+      {props.rating >= 1 && props.rating <= 5 ? <GiveRating rating={props.rating} /> : null}
+    </div>
+  );
+}
+
+function GiveRating(props){
+  return <h3>{stars[props.rating] - 1 }</h3>
 }
 
 export default RateARecipe;
